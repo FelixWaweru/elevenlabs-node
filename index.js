140e2b0d02a1c4e553913ccf39489bb9afd418f3
unknown <wawerufelix60@gmail.com>
2023-10-14 19:19:20 +0300

    feat: added speaker boost and variable constructor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const elevenLabsAPIV1 = "https://api.elevenlabs.io/v1";
-const elevenLabsAPIV2 = "https://api.elevenlabs.io/v2";
 
 
 /**
@@ -9,34 +8,18 @@ Creates an instance of ElevenLabs.
 @param {Object} - An object containing the API Key and API Version [default: ElevenLabs V1].
 */
 function ElevenLabs(options = {
-        apiKey: "",
-        apiVersion: ""
-    }) {
+    apiKey: "",
+    voiceId: ""
+}) {
 
     this.apiKey = options.apiKey ? options.apiKey : "";
-
-    switch(options.apiVersion) {
-      case "V1":
-        this.apiVersion = elevenLabsAPIV1;
-        break;
-      case "V2":
-        this.apiVersion = elevenLabsAPIV2;
-        break;
-      case "":
-        this.apiVersion = elevenLabsAPIV1;
-        break;
-      default:
-        console.log("Invalid Version");
-        break;
-    }
-  }
+    this.voiceId = options.voiceId ? options.voiceId : "ErXwobaYiN019PkySvjV";
+}
 
 
 /**
 
 Function that converts text to speech and saves the audio file to the specified file name.
-
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
 
 @param {string} fileName - The name of the file to save the audio data to.
 
@@ -48,65 +31,71 @@ Function that converts text to speech and saves the audio file to the specified 
 
 @param {string} modelId - The model to use for the text-to-speech conversion. If null, it will use elevenlab's default model.
 
+@param {boolean} speakerBoost - The speaker boost setting for the voice.
+
 @returns {Object} - An object containing the status of the operation.
 */
-ElevenLabs.prototype.textToSpeech = async function (
-  voiceID,
-  fileName,
-  textInput,
-  stability,
-  similarityBoost,
-  modelId
+ElevenLabs.prototype.textToSpeech = async function(
+    fileName,
+    textInput,
+    stability,
+    similarityBoost,
+    modelId,
+    speakerBoost
 ) {
-  try {
-    if (!voiceID || !fileName || !textInput) {
-      console.log("ERR: Missing parameter");
+    try {
+        if (!fileName) {
+            console.log("ERR: Missing parameter {fileName}");
+        } else if (!textInput) {
+            console.log("ERR: Missing parameter {textInput}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/text-to-speech/${this.voiceId}`;
+        const stabilityValue = stability ? stability : 0;
+        const similarityBoostValue = similarityBoost ? similarityBoost : 0;
+
+        const response = await axios({
+            method: "POST",
+            url: voiceURL,
+            data: {
+                text: textInput,
+                voice_settings: {
+                    stability: stabilityValue,
+                    similarity_boost: similarityBoostValue,
+                    use_speaker_boost: speakerBoost,
+                },
+                model_id: modelId ? modelId : undefined,
+            },
+            headers: {
+                Accept: "audio/mpeg",
+                "xi-api-key": this.apiKey,
+                "Content-Type": "application/json",
+            },
+            responseType: "stream",
+        });
+
+        response.data.pipe(fs.createWriteStream(fileName));
+
+        const writeStream = fs.createWriteStream(fileName);
+        response.data.pipe(writeStream);
+
+        return new Promise((resolve, reject) => {
+            const responseJson = {
+                status: "ok",
+                fileName: fileName
+            };
+            writeStream.on('finish', () => resolve(responseJson));
+
+            writeStream.on('error', reject);
+        });
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/text-to-speech/${voiceID}`;
-    const stabilityValue = stability ? stability : 0;
-    const similarityBoostValue = similarityBoost ? similarityBoost : 0;
-
-    const response = await axios({
-      method: "POST",
-      url: voiceURL,
-      data: {
-        text: textInput,
-        voice_settings: {
-          stability: stabilityValue,
-          similarity_boost: similarityBoostValue,
-        },
-        model_id: modelId ? modelId : undefined,
-      },
-      headers: {
-        Accept: "audio/mpeg",
-        "xi-api-key": this.apiKey,
-        "Content-Type": "application/json",
-      },
-      responseType: "stream",
-    });
-
-    response.data.pipe(fs.createWriteStream(fileName));
-
-    const writeStream = fs.createWriteStream(fileName);
-    response.data.pipe(writeStream);
-
-    return new Promise((resolve, reject) => {
-      const responseJson = { status: "ok", fileName: fileName };
-      writeStream.on('finish', () => resolve(responseJson));
-    
-      writeStream.on('error', reject);
-    });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
 
 Function that converts text to speech and returns a readable stream of the audio data.
-
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
 
 @param {string} textInput - The text to convert to speech.
 
@@ -118,48 +107,51 @@ Function that converts text to speech and returns a readable stream of the audio
 
 @param {string} responseType - The response type for the text-to-speech function (arrayBuffer, stream, etc). If null, it will use 'stream' by default.
 
+@param {boolean} speakerBoost - The speaker boost setting for the voice.
+
 @returns {Object} - A readable stream of the audio data.
 */
-ElevenLabs.prototype.textToSpeechStream = async function (
-  voiceID,
-  textInput,
-  stability,
-  similarityBoost,
-  modelId,
-  responseType
+ElevenLabs.prototype.textToSpeechStream = async function(
+    textInput,
+    stability,
+    similarityBoost,
+    modelId,
+    responseType,
+    speakerBoost
 ) {
-  try {
-    if (!voiceID || !textInput) {
-      console.log("ERR: Missing parameter");
+    try {
+        if (!textInput) {
+            console.log("ERR: Missing parameter {textInput}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/text-to-speech/${this.voiceId}/stream`;
+        const stabilityValue = stability ? stability : 0;
+        const similarityBoostValue = similarityBoost ? similarityBoost : 0;
+
+        const response = await axios({
+            method: "POST",
+            url: voiceURL,
+            data: {
+                text: textInput,
+                voice_settings: {
+                    stability: stabilityValue,
+                    similarity_boost: similarityBoostValue,
+                    use_speaker_boost: speakerBoost,
+                },
+                model_id: modelId ? modelId : undefined,
+            },
+            headers: {
+                Accept: "audio/mpeg",
+                "xi-api-key": this.apiKey,
+                "Content-Type": "application/json",
+            },
+            responseType: responseType ? responseType : "stream"
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/text-to-speech/${voiceID}/stream`;
-    const stabilityValue = stability ? stability : 0;
-    const similarityBoostValue = similarityBoost ? similarityBoost : 0;
-
-    const response = await axios({
-      method: "POST",
-      url: voiceURL,
-      data: {
-        text: textInput,
-        voice_settings: {
-          stability: stabilityValue,
-          similarity_boost: similarityBoostValue,
-        },
-        model_id: modelId ? modelId : undefined,
-      },
-      headers: {
-        Accept: "audio/mpeg",
-        "xi-api-key": this.apiKey,
-        "Content-Type": "application/json",
-      },
-      responseType: responseType ? responseType : "stream"
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
@@ -168,22 +160,22 @@ Function that returns an object containing the details for all the voices.
 
 @returns {Object} - An object containing the list of voices and their details.
 */
-ElevenLabs.prototype.getVoices = async function () {
-  try {
-    const voiceURL = `${this.apiVersion}/voices`;
+ElevenLabs.prototype.getVoices = async function() {
+    try {
+        const voiceURL = `${elevenLabsAPIV1}/voices`;
 
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 /**
@@ -192,116 +184,116 @@ Function that returns an object containing the default settings for the voices.
 
 @returns {Object} - An object containing the default settings for the voices.
 */
-ElevenLabs.prototype.getDefaultVoiceSettings = async function () {
-  try {
-    const voiceURL = `${this.apiVersion}/voices/settings/default`;
+ElevenLabs.prototype.getDefaultVoiceSettings = async function() {
+    try {
+        const voiceURL = `${elevenLabsAPIV1}/voices/settings/default`;
 
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-    });
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 /**
 
 Function that returns an object containing the settings of the specified voice.
 
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
+@param {string} voiceId - The ID of the voice to use for the text-to-speech conversion.
 
 @returns {Object} - An object containing the settings of the specified voice.
 */
-ElevenLabs.prototype.getVoiceSettings = async function (voiceID) {
-  try {
-    if (!voiceID) {
-      console.log("ERR: Missing parameter");
+ElevenLabs.prototype.getVoiceSettings = async function(voiceId) {
+    try {
+        if (!voiceId) {
+            console.log("ERR: Missing parameter {voiceId}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/voices/${voiceId}/settings`;
+
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/voices/${voiceID}/settings`;
-
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
 
 Function that returns an object containing the details of the specified voice.
 
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
+@param {string} voiceId - The ID of the voice to use for the text-to-speech conversion.
 
 @returns {Object} - An object containing the details of the specified voice.
 */
-ElevenLabs.prototype.getVoice = async function (voiceID) {
-  try {
-    if (!voiceID) {
-      console.log("ERR: Missing parameter");
+ElevenLabs.prototype.getVoice = async function(voiceId) {
+    try {
+        if (!voiceId) {
+            console.log("ERR: Missing parameter {voiceId}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/voices/${voiceId}`;
+
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/voices/${voiceID}`;
-
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
 
 Function that returns an object containing the status of the delete operation.
 
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
+@param {string} voiceId - The ID of the voice to use for the text-to-speech conversion.
 
 @returns {Object} - An object containing the status of the delete operation.
 */
-ElevenLabs.prototype.deleteVoice = async function (voiceID) {
-  try {
-    if (!voiceID) {
-      console.log("ERR: Missing parameter");
+ElevenLabs.prototype.deleteVoice = async function(voiceId) {
+    try {
+        if (!voiceId) {
+            console.log("ERR: Missing parameter {voiceId}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/voices/${voiceId}`;
+
+        const response = await axios({
+            method: "DELETE",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/voices/${voiceID}`;
-
-    const response = await axios({
-      method: "DELETE",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
 
 Function that returns an object containing the status of the edit operation.
 
-@param {string} voiceID - The ID of the voice to use for the text-to-speech conversion.
+@param {string} voiceId - The ID of the voice to use for the text-to-speech conversion.
 
 @param {number} stability - The stability setting for the voice.
 
@@ -309,36 +301,36 @@ Function that returns an object containing the status of the edit operation.
 
 @returns {Object} - An object containing the status of the edit operation.
 */
-ElevenLabs.prototype.editVoiceSettings = async function (
-  voiceID,
-  stability,
-  similarityBoost
+ElevenLabs.prototype.editVoiceSettings = async function(
+    voiceId,
+    stability,
+    similarityBoost
 ) {
-  try {
-    if (!voiceID) {
-      console.log("ERR: Missing parameter");
+    try {
+        if (!voiceId) {
+            console.log("ERR: Missing parameter {voiceId}");
+        }
+
+        const voiceURL = `${elevenLabsAPIV1}/voices/${voiceId}/settings/edit`;
+        const stabilityValue = stability ? stability : 0;
+        const similarityBoostValue = similarityBoost ? similarityBoost : 0;
+
+        const response = await axios({
+            method: "POST",
+            url: voiceURL,
+            data: {
+                stability: stabilityValue,
+                similarity_boost: similarityBoostValue,
+            },
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-
-    const voiceURL = `${this.apiVersion}/voices/${voiceID}/settings/edit`;
-    const stabilityValue = stability ? stability : 0;
-    const similarityBoostValue = similarityBoost ? similarityBoost : 0;
-
-    const response = await axios({
-      method: "POST",
-      url: voiceURL,
-      data: {
-        stability: stabilityValue,
-        similarity_boost: similarityBoostValue,
-      },
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 /**
@@ -347,22 +339,22 @@ Function that returns an object containing the list of voice models.
 
 @returns {Object} - An object containing the list of voice models and their details.
 */
-ElevenLabs.prototype.getModels = async function () {
-  try {
-    const voiceURL = `${this.apiVersion}/models`;
+ElevenLabs.prototype.getModels = async function() {
+    try {
+        const voiceURL = `${elevenLabsAPIV1}/models`;
 
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 /**
@@ -371,22 +363,22 @@ Function that returns the user details.
 
 @returns {Object} - An object containing the user details.
 */
-ElevenLabs.prototype.getUserInfo = async function () {
-  try {
-    const voiceURL = `${this.apiVersion}/user`;
+ElevenLabs.prototype.getUserInfo = async function() {
+    try {
+        const voiceURL = `${elevenLabsAPIV1}/user`;
 
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 /**
@@ -395,22 +387,22 @@ Function that returns the user subscription details.
 
 @returns {Object} - An object containing the user subscription details.
 */
-ElevenLabs.prototype.getUserSubscription = async function () {
-  try {
-    const voiceURL = `${this.apiVersion}/user/subscription`;
+ElevenLabs.prototype.getUserSubscription = async function() {
+    try {
+        const voiceURL = `${elevenLabsAPIV1}/user/subscription`;
 
-    const response = await axios({
-      method: "GET",
-      url: voiceURL,
-      headers: {
-        "xi-api-key": this.apiKey,
-      },
-    });
+        const response = await axios({
+            method: "GET",
+            url: voiceURL,
+            headers: {
+                "xi-api-key": this.apiKey,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 module.exports = ElevenLabs;
